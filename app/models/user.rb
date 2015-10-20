657e7ee959a1df600657a6b7f62588766a6bbc0f
Justin Liman <justininternetstuff@hotmail.com>
2015-10-20 08:15:35 +0000

    Signup

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  before_save { self.username = username.downcase }
+  validates :username,  presence: true, length: { maximum: 50 },
+                        uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+end
