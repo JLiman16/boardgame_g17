2168fb6614bf3e0b555e0949c7c462daae3f919b
Justin Liman <justininternetstuff@hotmail.com>
2015-10-20 09:21:49 +0000

    Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
   root 'users#new'
-  #get 'signup'  => 'users#new'
+  get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  get 'signup'  => 'users#new'
   
   resources :users
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020071905) do
+ActiveRecord::Schema.define(version: 20151020084847) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["username"], name: "index_users_on_username", unique: true
