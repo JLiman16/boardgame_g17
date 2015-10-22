013cc173ff5d29efe6959a9a71b8bd5cad5e4da2
epithemeus <banemalar7@hotmail.com>
2015-10-22 05:01:26 +0000

    Tests added

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,54 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Username exists" do
+   user = User.new(
+    username: "",
+    password: "Thisisjustatest",
+    password_confirmation: "Thisisjustatest"
+    )
+    assert_not user.valid?
+  end
+  
+  test "Username unique" do
+    user = User.new(
+      username: "Testing",
+      password: "Thisisjustatest",
+      password_confirmation: "Thisisjustatest"
+      )
+    user2 = User.new(
+      username: "Testing",
+      password: "Thisisjustatest",
+      password_confirmation: "Thisisjustatest"
+      )
+      user.save
+    assert_not user2.valid?
+  end
+  
+  test "Password exists" do
+    user = User.new(
+      username: "Testing",
+      password: "",
+      password_confirmation: ""
+      )
+    assert_not user.valid?
+  end
+  
+  test "Password length sufficient" do
+    user = User.new(
+      username: "Testing",
+      password: "five",
+      password_confirmation: "five"
+      )
+    assert_not user.valid?
+  end
+  
+  test "Password confirmation matches" do
+    user = User.new(
+      username: "Testing",
+      password: "Thefroggoes",
+      password_confirmation: "Hippedyhop"
+      )
+    assert_not user.valid?
+  end
 end
