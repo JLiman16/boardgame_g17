6717b1eef0882ca7b5d3a69d9abf8fe7dd592d46
Justin Liman <justininternetstuff@hotmail.com>
2015-11-11 00:57:36 +0000

    Changed the homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
   def full_title(page_title = '')
-    base_title = "Bored? Game!"
+    base_title = "Bored?Game!"
     if page_title.empty?
       base_title
     else
