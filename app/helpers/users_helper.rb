898b8b5a6f3c197d04cd2607660542022ca3f922
epithemeus <banemalar7@hotmail.com>
2015-11-27 07:46:12 +0000

    Fixed sorting mislinking

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
     def sortable(title, column)
         direction = column == params[:sort] && params[:direction] == "asc" ? "desc" : "asc"
-        link_to title, :sort => column, :direction => direction
+        link_to title, find_game_path(@user, :sort => column, :direction => direction)
     end
 end
