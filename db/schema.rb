ba7d35645782f77c7badec2164ec9218a6c2ae20
epithemeus <banemalar7@hotmail.com>
2015-11-30 21:36:57 +0000

    Goddamit Schema.rb!

--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(version: 20151128220841) do
   add_index "categories_games", ["category_id"], name: "index_categories_games_on_category_id"
   add_index "categories_games", ["game_id"], name: "index_categories_games_on_game_id"
 
+  create_table "collections", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false
     t.integer  "attempts",   default: 0, null: false
@@ -55,6 +60,7 @@ ActiveRecord::Schema.define(version: 20151128220841) do
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
 
   create_table "games", force: :cascade do |t|
+    t.integer  "bgg_account_id"
     t.string   "bggid"
     t.string   "bgname"
     t.integer  "yearpublished"
@@ -72,11 +78,8 @@ ActiveRecord::Schema.define(version: 20151128220841) do
     t.string   "thumbnail"
   end
 
-<<<<<<< HEAD
-=======
   add_index "games", ["bgg_account_id"], name: "index_games_on_bgg_account_id"
   
->>>>>>> a85ceb133943a0555363374268e673a2c98e7dfa
   create_table "games_mechanics", id: false, force: :cascade do |t|
     t.integer "mechanic_id"
     t.integer "game_id"
